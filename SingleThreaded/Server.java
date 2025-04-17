3599726ca9b25b8a90ea5f2a435495b1596bb0cd
TheAyushB <ayushbhardwaj510@gmail.com>
2025-04-17 22:00:21 +0530

    closed socket

--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -10,15 +10,18 @@ public class Server {
     
     public void run() throws IOException, UnknownHostException{
         int port = 8010;
-        ServerSocket socket = new ServerSocket(port);
-        socket.setSoTimeout(20000);
-        while(true){
-            System.out.println("Server is listening on port: "+port);
-            Socket acceptedConnection = socket.accept();
-            System.out.println("Connected to "+acceptedConnection.getRemoteSocketAddress());
-            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
-            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
-            toClient.println("Hello World from the server");
+        try (ServerSocket socket = new ServerSocket(port)) {
+            socket.setSoTimeout(20000);
+            while(true){
+                System.out.println("Server is listening on port: "+port);
+                Socket acceptedConnection = socket.accept();
+                System.out.println("Connected to "+acceptedConnection.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                toClient.println("Hello World from the server");
+            }
+        } catch(Exception ex) {
+            System.err.println(ex);
         }
     }
 
